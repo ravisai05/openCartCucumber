204e4a6be8e7bb45f2c6a9bb1af93aecce3401c2
ravisai05 <ravisai.saridevi@gmail.com~>
2023-12-02 23:41:27 +0530

    second commit

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {".//Features//LoginFeatureUsingExcel.feature"},
+    features = {".\\Features\\Loginoutline.feature",".//Features//LoginFeatureUsingExcel.feature"},
     glue = "stepDefinition",
-    plugin= {"pretty","html:reports/myreport.html","json:reports/myreport.json"},
+    plugin= {"pretty","html:reports/myreport.html","json:reports/myreport.json","rerun:target/rerun.txt"},
     dryRun = false,
     monochrome = true,
     tags="@sanity"
